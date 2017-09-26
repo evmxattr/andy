3023b95596e66f44d7c13c1bfc37cff8a7d0c468
none <none>
2017-09-26 22:30:09 +0200

    splash

--- cli.py
+++ cli.py
@@ -16,6 +16,19 @@ import subprocess
 from blindspin import spinner
 
 
+splash = """
+  ______                                 __     __
+ /      \                               /  |   /  |
+/$$$$$$  |______    ______    ______   _$$ |_  $$ |
+$$ |_ $$//      \  /      \  /      \ / $$   | $$ |
+$$   |  /$$$$$$  |/$$$$$$  |/$$$$$$  |$$$$$$/  $$ |
+$$$$/   $$ |  $$/ $$ |  $$ |$$ |  $$ |  $$ | __$$/
+$$ |    $$ |      $$ \__$$ |$$ \__$$ |  $$ |/  |__
+$$ |    $$ |      $$    $$/ $$    $$/   $$  $$//  |
+$$/     $$/        $$$$$$/   $$$$$$/     $$$$/ $$/
+                                                   
+"""
+
 def format_help(help):
     """Formats the help string."""
     help = help.replace('Options:', str(crayons.white('Options:', bold=True)))
@@ -23,8 +36,7 @@ def format_help(help):
     help = help.replace('Usage: froot', str(
         'Usage: {0}'.format(crayons.white('froot', bold=True))))
 
-    help = help.replace('  install', str(
-        crayons.yellow('  install', bold=True)))
+    help = help.replace('  install', str(crayons.yellow('  install', bold=True)))
     help = help.replace('  root', str(crayons.red('  root', bold=True)))
     help = help.replace('  shell', str(crayons.blue('  shell', bold=True)))
 
@@ -33,16 +45,9 @@ Usage Examples:
    A:
    $ {0}
 
-   B:
-   $ {1}
-
-   C:
-   $ {2}
 
 Commands:""".format(
         crayons.red('froot --A'),
-        crayons.red('froot --X'),
-        crayons.red('froot install --dev'),
     )
 
     help = help.replace('Commands:', additional_help)
@@ -54,7 +59,7 @@ Commands:""".format(
     ignore_unknown_options=True,
     allow_extra_args=True
 ))
-@click.argument('package', default=False)
+@click.argument('package', default=None)
 @click.option('--device', default=None, help="Specify which device to get shell in")
 @click.option('--verbose', is_flag=True, default=False, help="Verbose mode.")
 def install(package=None, device=None, verbose=False):
@@ -65,12 +70,15 @@ def install(package=None, device=None, verbose=False):
         dev = "-s {device}".format(device=device)
 
     command = "adb {extra} install {package}".format(extra=dev, package=package).split()
-    res = subprocess_with_output(command)
-    ret_msg = res.splitlines()[-1]
-    if 'Success' in ret_msg:
-        print(str(crayons.yellow('Package was installed', bold=True)))
+    try:
+        res = subprocess_with_output(command).splitlines()[-1]
+    except:
+        pass
     else:
-        print(str(crayons.red('Unable to install package: {}'.format(ret_msg), bold=True)))
+        if 'Success' in res:
+            print(str(crayons.yellow('Package was installed', bold=True)))
+        else:
+            print(str(crayons.red('Unable to install package: {}'.format(res), bold=True)))
 
 
 @click.command(help="Spawn a shell on the device.", context_settings=dict(
@@ -97,6 +105,7 @@ def subprocess_with_output(command, newlines=True):
 ))
 @click.option('--verbose', is_flag=True, default=False, help="Verbose mode.")
 def devices(verbose=False):
+    print(str(crayons.green('Available devices', bold=True)))
     output = subprocess_with_output(["adb", "devices"])
     for line in output.splitlines()[1:]:
         print(line.split('\t')[0])
@@ -114,15 +123,14 @@ def root(
 
 
 @click.group(invoke_without_command=True)
-@click.option('--update', is_flag=True, default=False, help="Update to latest.")
-@click.option('--python', default=False, nargs=1, help="Specify which version of Python virtualenv should use.")
 @click.option('--help', '-h', is_flag=True, default=None, help="Show this message then exit.")
 @click.version_option(prog_name=crayons.yellow('froot'), version="0.0.1")
 @click.pass_context
 def cli(
-        ctx, help=False, update=False, python=False):
+        ctx, help=False):
 
     if ctx.invoked_subcommand is None:
+        click.echo(crayons.white(splash, bold=True))
         click.echo(format_help(ctx.get_help()))
 
 
